bc836ca4bfc5fb094b41974c40c5f65328b5daac
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-09-12 20:15:27 +0530

    added loader to explore page

--- src/Pages/People/People.js
+++ src/Pages/People/People.js
@@ -1,13 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Navbar from "../../Components/Navbar/Navbar";
 import PeopleCard from "../../Components/People/PeopleCard";
 import Rightbar from "../../Components/Rightbar/Rightbar";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import { useScrollToTop } from "../../Hooks/UseScrollToTop";
 import "./people.style.css";
+import { CircularProgress } from "@material-ui/core";
 
 const People = () => {
-  const { allUsers, userInfo: authUser } = useSelector((state) => state.user);
+  const {
+    allUsers,
+    pending,
+    userInfo: authUser,
+  } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const allPeople = allUsers.filter((people) => people._id !== authUser._id);
 
@@ -19,14 +25,20 @@ const People = () => {
       <div className="flex w-full">
         <Sidebar />
         <div className="post-details">
-          <div className="people-details-wrapper">
-            <h2 className="font sans text-xl md:text-3xl mt-2 ">
-              People You may know
-            </h2>
-            {allPeople.map((item) => (
-              <PeopleCard person={item} key={item._id} />
-            ))}
-          </div>
+          {pending ? (
+            <div className="flex justify-center items-center h-80">
+              <CircularProgress color="secondary" />
+            </div>
+          ) : (
+            <div className="people-details-wrapper">
+              <h2 className="font sans font-semibold text-xl md:text-3xl mt-2 ">
+                People You may know
+              </h2>
+              {allPeople.map((item) => (
+                <PeopleCard person={item} key={item._id} />
+              ))}
+            </div>
+          )}
         </div>
         <Rightbar />
       </div>
